f4d63739ce1c7b8fb25b7416e8cb3b799ee68f91
pkwok123 <peiyikwok@gmail.com>
2020-06-07 22:25:28 -1000

    fix routing

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use("/api/items", items);
 
 app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 // app.get("/", (req, res) => {
